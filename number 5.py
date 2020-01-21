427648e8b8ce5347e79c54d0a41b67716b16ded7
K-mahmoudi <59234192+K-mahmoudi@users.noreply.github.com>
2020-01-21 13:19:54 +0330

    Add files via upload

---
+++ number 5.py
@@ -0,0 +1,16 @@
+k = 'kousha.text'
+k1 ='kousha 1.text'
+#with open(k, 'r') as infile:
+  # words = infile.read()
+copyright(k) to (k1)
+doIHaveToCopyTheLine=False
+
+#for line in k.readlines():
+
+  #  if 'tests/file/myword' in line:
+     #   doIHaveToCopyTheLine=True
+
+   # if doIHaveToCopyTheLine:
+    #    k1.write(line)
+
+#k1.close()
